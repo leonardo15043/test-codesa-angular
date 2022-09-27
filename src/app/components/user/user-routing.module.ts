bcbac1b2fd580b4acd215e666f89a1bb84deb351
leonardo15043 <lahernandez15043@gmail.com>
2022-09-27 13:09:26 -0500

    Layout [ User list, User action ]

--- src/app/components/user/user-routing.module.ts
+++ src/app/components/user/user-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './pages/user-list/user-list.component';
+import { UserActionComponent } from './pages/user-action/user-action.component';
 
 const routes: Routes = [
   {
     path: 'list',
     component: UserListComponent
+  },
+  {
+    path: 'action/:type',
+    component: UserActionComponent
   }
 ];
 
--- src/app/components/user/user.module.ts
+++ src/app/components/user/user.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UserListComponent } from './pages/user-list/user-list.component';
 import { UserRoutingModule } from './user-routing.module';
+import { UserActionComponent } from './pages/user-action/user-action.component';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { UserRoutingModule } from './user-routing.module';
     UserRoutingModule
   ],
   declarations: [
-    UserListComponent
+    UserListComponent,
+    UserActionComponent
   ]
 })
 export class UserModule { }
